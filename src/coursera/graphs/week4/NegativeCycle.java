22b2ea8c9930b9731106dacba00bd4c144724162
Aman Adhikari <adhikariaman01@gmail.com>
2016-11-13 14:37:41 -0600

    Graph Week 4

---
+++ src/coursera/graphs/week4/NegativeCycle.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2016. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package coursera.graphs.week4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class NegativeCycle {
+    private static int negativeCycle(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost) {
+        // write your code here
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+        ArrayList<Integer>[] cost = (ArrayList<Integer>[])new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<Integer>();
+            cost[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < m; i++) {
+            int x, y, w;
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            w = scanner.nextInt();
+            adj[x - 1].add(y - 1);
+            cost[x - 1].add(w);
+        }
+        System.out.println(negativeCycle(adj, cost));
+    }
+}
+
