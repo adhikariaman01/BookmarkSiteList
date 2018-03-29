897f79c9cd5a4ed8e936c3bb489233894fed4d1d
Aman Adhikari <adhikariaman01@gmail.com>
2018-03-28 22:19:58 -0500

    SingleLikedList

---
+++ MyBookmarkedLink/Java/LinkedList/SinglyLinkedList.java
@@ -0,0 +1,68 @@
+/** 
+Implementation from Data Structure and Algorithm using Java
+**/
+public class SinglyLinkedList<E> {
+	private static class Node<E>{
+		private E element;
+		private Node<E> next;
+
+		public Node(E e,Node<E> n){
+			element = e;
+			next = n;
+		}
+
+		public E getElement() { return element;}
+		public Node<E> getNext(){ return next; }
+		public void setNext(Node<E> n){ next = n; }
+	}
+	// Instance varaible of SIngly LinkedList
+	private Node<E> head = null;
+	private Node<E> tail = null;
+	private size = 0;
+	public SinglyLinkedList(){
+
+	}
+
+	public int size(){ return size; }
+
+	public boolean isEmpty(){ return size == 0; }
+
+	public E first(){
+		if(isEmpty()){ return null; }
+		return head.getElement();
+	}
+
+	public E last(){
+		if(isEmpty()){ return null; }
+		return tail.getElement();
+	}
+
+	public void addFirst(E e){
+		head = new Node<>(e,head);
+		if(size == 0){
+			tail = head;
+		}
+		size++;
+	}
+
+	public addlast(E e){
+		Node<E> newest = new Node<>(e,null);
+		if(isEmpty()){
+			head = newest;
+		}else{
+			tail.setNext(newest);
+		}
+		tail = newest;
+	}
+
+	public E removeFirst(){
+		if(isEmpty()){ return null;}
+		E answer = head.getElement();
+		head = head.getNext();
+		size--;
+		if(size == 0){
+			tail = null;
+		}
+		return answer;
+	}
+}
